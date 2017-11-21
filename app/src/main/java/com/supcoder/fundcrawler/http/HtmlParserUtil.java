eb8173fe5b0e423291722c6d9398c766de4265cc
huqi <huqi.opop@qq.com>
2017-11-21 11:55:12 +0800

    fix

--- app/src/main/java/com/supcoder/fundcrawler/http/HtmlParserUtil.java
+++ app/src/main/java/com/supcoder/fundcrawler/http/HtmlParserUtil.java
@@ -37,6 +37,18 @@ public class HtmlParserUtil {
         }
     }
 
+    /**
+     * 提供精确的乘法运算。
+     * @param v1 被乘数
+     * @param v2 乘数
+     * @return 两个参数的积
+     */
+    public  double mul(String v1,String v2){
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
+        return b1.multiply(b2).doubleValue();
+    }
+
     public List<ProcessMessege> queryProcessInfo2(String fundCode) {
         String url = "http://fund.eastmoney.com/" + fundCode + ".html?spm=aladin";
         Document doc = null;
